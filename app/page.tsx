8b3420a9bfe273a82ed8a1868ff992c2dde26825
nova-ansh <ansh42deep@gmail.com>
2026-09-24 09:31:32 +0530

    Correction in TOD - 24/IX/1806

--- app/page.tsx
+++ app/page.tsx
@@ -452,7 +452,7 @@ export default function Home() {
                         md:text-[27px]
                       "
                     >
-                      TOD: Only (99) days are left in 2026 - what are you waiting for? Go, fulfill yout New Year Resolutions that you made (266) days ago. Still time.
+                      TOD: Only (99) days are left in 2026 - what are you waiting for? Go, fulfill your New Year Resolutions that you made (266) days ago. Still time.
                     </p>
 
 
